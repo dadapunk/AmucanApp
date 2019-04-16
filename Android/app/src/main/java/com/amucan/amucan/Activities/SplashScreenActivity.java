10d3a9ae4e04cee28ee9310a76bcc11aa45beac5
Sebastian Velasquez <sebastian.v.a@riseup.net>
2019-04-16 18:08:07 -0300

    flow after splah fixed

--- Android/app/src/main/java/com/amucan/amucan/Activities/SplashScreenActivity.java
+++ Android/app/src/main/java/com/amucan/amucan/Activities/SplashScreenActivity.java
@@ -55,7 +55,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void dismissSplash() {
-        startActivity(new Intent(SplashScreenActivity.this, MapActivity.class));
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
         finish();
     }
 
